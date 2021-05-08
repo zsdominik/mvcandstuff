5de9985690cf2f72608053670dc272076affa5ac
Dominik Zsiros <zsdominik@outlook.com>
2021-05-08 12:14:23 +0200

    init commit

---
+++ src/main/java/com/zsirosd/mvcandstuff/MvcandstuffApplication.java
@@ -0,0 +1,13 @@
+package com.zsirosd.mvcandstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcandstuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcandstuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zsirosd/mvcandstuff/MvcandstuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsirosd.mvcandstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcandstuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
