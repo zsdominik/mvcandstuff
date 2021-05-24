6bc7603bd8dcea497885f9c567d5706672fe82af
mahegedus <mate_hegedus@epam.com>
2021-05-24 19:22:19 +0200

    add additional views and resolvers

--- src/main/java/com/zsirosd/mvcandstuff/controller/HomeController.java
+++ src/main/java/com/zsirosd/mvcandstuff/controller/HomeController.java
@@ -2,12 +2,24 @@ package com.zsirosd.mvcandstuff.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
+    @ResponseBody
     public String home() {
+        return "home";
+    }
+
+    @GetMapping("/custom")
+    public String custom() {
+        return "custom";
+    }
+
+    @GetMapping("/homejsp")
+    public String homejsp() {
         return "home";
     }
 }
